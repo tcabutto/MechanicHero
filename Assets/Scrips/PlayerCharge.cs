3fd7fd537ef94e94d0aa0e21727b5fefecacc6b4
harleyhash <theofficialhrhash@gmail.com>
2022-12-16 23:08:26 -0500

    Added a player Health Bar that goes changes on Collision with other Colliders

---
+++ Assets/Scrips/PlayerCharge.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCharge : MonoBehaviour
+{
+    //Slider healthbar tells sets which Slider to modify
+    [SerializeField] Slider healthbar;
+    //Sets Starting health value to 100
+    private float health_value = 100;
+    //Stores changed health value
+    private float current_health;
+
+    //On start sets health value
+    void Start()
+    {
+        current_health = health_value;
+    }
+
+    //On Collision subtracts health every time player Collides
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        current_health -= 32;
+        healthbar.value = current_health;
+    }
+}
