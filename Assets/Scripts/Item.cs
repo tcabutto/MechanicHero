08ddb3c4508b6e5f6eb135dfbc0ae8533dd5d7e5
tcabutto <tcabutto@gmail.com>
2022-12-18 13:25:27 -0500

    Added enum biome with flags in case item can be found in only 2 of 4 biomes for example.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,29 @@ public class Item : MonoBehaviour
     public int id;
     public string title;
     public string description;
-    string biome;
     public Sprite icon;
+    public Biome biome;
+    
+    [Flags] //example:  Item item = new Item("Battery", "A battery to charge your car", Item.Biome.Forest | Item.Biome.Desert);
+    public enum Biome
+    {
+      All,
+      Desert,
+      Forest,
+      City, 
+      Mountains,
+      Fallout,
+      Cave
+    }
     //public Dictionary<string, int> stats = new Dictionary<string, int>();  //will revisit if this is needed. Not sure yet
 
-  public Item(int id, string title, string description, string biome)
+  public Item(int id, string title, string description, Biome biome)
   {
     this.id = id;
     this.title = title;
     this.description = description;
     this.biome = biome;
-    this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+    this.icon = Resources.Load<Sprite>("Art/Sprites/Items/" + title);
     
   }
 
@@ -26,7 +39,7 @@ public class Item : MonoBehaviour
     this.id = item.id;
     this.title = item.title;
     this.description = item.description;
-    this.icon = Resources.Load<Sprite>("Sprites/Items/" + item.title);
+    this.icon = Resources.Load<Sprite>("Art/Sprites/Items/" + item.title);
     this.biome = item.biome;
   }
     
