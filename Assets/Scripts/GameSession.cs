d8dadfda46f227f1c7c7b5094da3f80e9d59e186
tcabutto <tcabutto@gmail.com>
2022-12-18 13:18:57 -0500

    Added skeleton code for managing inventory with db or file at later date. Added Player and a list of Garage Inventories

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections.Generic;
+
 public class GameSession : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI moneyText;
     [SerializeField] float loadDelay = 3f;
 
-    Inventory inventory;
+    Inventory PlayerInventory;
+
+    public List<GarageInventory> GarageInventories;
 
     
     private void Awake()
@@ -18,14 +22,44 @@ public class GameSession : MonoBehaviour
      }
      else
      {
-        inventory = FindObjectOfType<Inventory>();
+          // Load the size of the player inventory from a file or a database.
+        int playerInventorySize = LoadPlayerInventorySize();
+
+        // Create an instance of the PlayerInventory class with the loaded size.
+        PlayerInventory = new PlayerInventory(playerInventorySize);
+
+        // Load the sizes of the garage inventories from a file or a database.
+        List<int> GarageInventorySizes = LoadGarageInventorySizes();
+
+        // Create a list of garage base inventories.
+        GarageInventories = new List<GarageInventory>();
+
+        // Add instances of the GarageInventory class with the loaded sizes to the list.
+        foreach (int size in GarageInventorySizes)
+        {
+            GarageInventories.Add(new GarageInventory(size));
+        }
+            // Loads the size of the player inventory from a file or a database.
+    int LoadPlayerInventorySize()
+    {
+        // TODO: Load the size of the player inventory from a file or a database.
+        return 0;
+    }
+
+    // Loads the sizes of the garage base inventories from a file or a database.
+    List<int> LoadGarageInventorySizes()
+    {
+        // TODO: Load the sizes of the garage base inventories from a file or a database.
+        return new List<int>();
+    }
+
         DontDestroyOnLoad(gameObject);
      }
     }
 
     private void Start() 
     {
-        moneyText.text = "$ " + inventory.money.ToString();
+        moneyText.text = "$ " + PlayerInventory.GetMoney().ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -40,9 +74,5 @@ public class GameSession : MonoBehaviour
         Destroy(gameObject);
     }
 
-    public void IncrementMoney(decimal value)
-    {
-        inventory.money += value;
-        moneyText.text = "Money: " + inventory.money.ToString();
-    }
+    
 }
