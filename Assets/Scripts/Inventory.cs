3ec9f28502495205dd7d5e184509394524331274
tcabutto <tcabutto@gmail.com>
2022-12-18 00:50:59 -0500

    Created basic inventory class to be expanded upon later

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public decimal money = 0.00m;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
